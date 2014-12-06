f1c597e3a6c875d38d4d4677a0e7d0de3e4d190d
Damon Barry <damonbarry@outlook.com>
2014-12-05 19:05:40 -0800

    create the sample app which uses the number class to find digits

--- sample/sample.cpp
+++ sample/sample.cpp
@@ -1,11 +1,54 @@
-// sample.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
+#include <iostream>
+#include <sstream>
+#include <array>
+#include "..\find_digits.hpp"
 
+using namespace std;
+using namespace find_digits;
 
-int _tmain(int argc, _TCHAR* argv[])
+int main()
 {
-	return 0;
+    cout << "Enter number of tests [1 <= T <= 15]: ";
+
+    size_t num_args;
+    cin >> num_args;
+    cin.sync();
+
+    if (num_args == 0 || num_args > 15)
+    {
+        cout << "T out of range\n";
+        return 1;
+    }
+
+    vector<string> args;
+
+    for (size_t i = 0; i < num_args; ++i)
+    {
+        const size_t max_digits = 10;
+        const size_t buffer_len = max_digits + 1 /* so we know if user entered too many chars */ + 1 /* null terminator */;
+        array<char, buffer_len> value;
+
+        cout << "(" << i + 1 << ") Enter a number: ";
+
+        cin.getline(value.data(), buffer_len);
+
+        if (string("0") == value.data() || strlen(value.data()) > max_digits)
+        {
+            cout << "N out of range\n";
+            return 1;
+        }
+
+        args.push_back(std::move(value.data()));
+    }
+
+    cout << "\nResults:\n";
+
+    for (auto arg : args)
+    {
+        cout << number(arg).count_divisor_digits() << "\n";
+    }
+
+    return 0;
 }
 
